3afca7cdcc8a62f5b4dd0aa156d7ef2837806c8f
Bola382e <marcus.gabriel.silva@hotmail.com>
2023-02-22 21:46:49 -0300

    Importing data into R

---
+++ 1 - Saving data.R
@@ -0,0 +1,23 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list=ls())
+# reading dataset
+
+raw_data = read.table("data/column_3C.dat",h=F,sep=" ",dec='.')
+View(raw_data)
+
+colnames(raw_data) = c("pelIncidence",
+                   "pelTilt",
+                   "lumbLordAngle",
+                   "sacrSlope", 
+                   "pelRadius", 
+                   "grdSpondy",
+                   "class")
+
+cnst_n = nrow(raw_data) # n obs
+cnst_p = ncol(raw_data)-1 # n features
+
+# dataset about vertebral column deformities
+# description given on https://archive.ics.uci.edu/ml/datasets/Vertebral+Column
+# target variable on last column
+
+save.image("Data.RData")
