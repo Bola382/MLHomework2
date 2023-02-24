f56d6d614f0642d18f9892c1f639f411e42f6d9d
Bola382e <marcus.gabriel.silva@hotmail.com>
2023-02-24 19:25:48 -0300

    LOO CV for selected features

--- 4 - Leave-one-out CV.R
+++ 4 - Leave-one-out CV.R
@@ -2,98 +2,83 @@ rm(list=ls())
 load("var select.RData")
 library(rminer);suppressMessages(library(dplyr))
 
-X = data %>% select(-last_col())                                  # covariates
-Y = data %>% select(last_col()) %>% unlist %>% rmnames %>% factor # target
-n  = nrow(X)                                                      # sample size
-C  = length(unique(Y))                                            # number of classes
-m  = 100                                                          # mc replicas
+dat_class = dat_class$class %>% factor # target
+cnst_n  = nrow(dat_features)           # sample size
+temp_C  = length(unique(dat_class))    # number of classes
+temp_M  = 1                            # mc replicas
+temp_regress = dat.err_regress         # selected vars
 
-k    = 10   # number of partitions (k=n leave one out)
-# FOR LEAVE ONE OUT m MUST BE EQUAL TO ONE
-# SINCE THERE IS NO VARIABILITY IN THE ERROR RATE
-CV = rep(1:k,each=trunc(n/k),length.out=n) # partitions
-methods = c("5NN","10NN","15NN","LDA","DT","SVM","MULTINOM")
-lmeth = length(methods) # number of methods used
+temp_k    = cnst_n   # number of partitions (temp_k=cnst_n leave one out)
+# FOR LEAVE ONE OUT temp_M MUST BE EQUAL TO ONE
+# SINCE THERE IS NO VARIABILITY IN THE METRICS
+temp_CV = rep(1:temp_k,each=trunc(cnst_n/temp_k),length.out=cnst_n) # partitions
+temp_methods = c("LDA","QDA","NBB")
+temp_lmeth = length(temp_methods) # number of temp_methods used
 
-tx_erro = matrix(NA, m, lmeth, # stores error rate
-                 dimnames = list(1:m,methods))
+dat_txerr = matrix(NA, temp_M, temp_lmeth, # stores error rate
+                 dimnames = list(1:temp_M,temp_methods))
 # progress bar
-pb <- txtProgressBar(min = 0,max = m,style = 3,width = 50,char = "=") 
+temp_pb <- txtProgressBar(min = 0,max = temp_M,style = 3,width = 50,char = "=") 
 
 tm = proc.time()[3]
 set.seed(1)
 # mc loop
-for(i in 1:m){
- # randomizes the CV partitions
- idCV = sample(CV) 
+for(i in 1:temp_M){
+ # randomizes the temp_CV partitions
+ temp_idCV = sample(temp_CV) 
  
- Ypred5NN  = NULL
- Ypred10NN = NULL
- Ypred15NN = NULL
- YpredLDA  = NULL
- YpredDT   = NULL
- YpredSVM  = NULL
- YpredMULTINOM  = NULL
+ temp_YpredLDA  = NULL
+ temp_YpredQDA  = NULL
+ temp_YpredNBB  = NULL
  
- pb2 <- txtProgressBar(min = 0,max = k,style = 3,width = 25,char = "-") 
+ pb2 <- txtProgressBar(min = 0,max = temp_k,style = 3,width = 25,char = "-") 
  # sets each partition as training set w/ the others as test sets
- for(g in 1:k){
+ for(g in 1:temp_k){
   
-  Xtr = X[idCV!=g,]
-  Xts = X[idCV==g,]
-  Ytr = Y[idCV!=g]
-  Yts = Y[idCV==g]
+  temp_Xtr = dat_features[temp_idCV!=g,]
+  temp_Xts = dat_features[temp_idCV==g,]
+  temp_Ytr = dat_class[temp_idCV!=g]
+  temp_Yts = dat_class[temp_idCV==g]
   
   # check for variance in the class
-  if(length(unique(Y[idCV!=g])) != C){
+  if(length(unique(dat_class[temp_idCV!=g])) != temp_C){
    break
   }
   # training set
-  dbtreino = cbind.data.frame(Xtr, classe = Ytr)
+  dbtreino = cbind.data.frame(temp_Xtr, classe = temp_Ytr)
   # test set
-  dbteste  = cbind.data.frame(Xts, classe = Yts)
+  dbteste  = cbind.data.frame(temp_Xts, classe = temp_Yts)
   
   # model fit
-  mod5NN   = fit(classe ~ ., data = dbtreino[,c(58,regress[[1]])],
-                 model = "knn", task = "c", k =5)
-  mod10NN   = fit(classe ~ ., data = dbtreino[,c(58,regress[[2]])],
-                  model = "knn", task = "c", k =10)
-  mod15NN   = fit(classe ~ ., data = dbtreino[,c(58,regress[[3]])],
-                  model = "knn", task = "c", k =15)
-  modLDA   = fit(classe ~ ., data = dbtreino[,c(58,regress[[4]])],
+  temp_modLDA   = fit(classe ~ ., data = dbtreino[,c(7,temp_regress[[1]])],
                  model = "lda", task = "c")
-  modDT   = fit(classe ~ ., data = dbtreino[,c(58,regress[[5]])],
-                model = "dt", task = "c")
-  modSVM   = fit(classe ~ ., data = dbtreino[,c(58,regress[[6]])],
-                 model = "svm", task = "c")
-  modMULTINOM   = fit(classe ~ ., data = dbtreino[,c(58,regress[[7]])],
-                      model = "multinom", task = "c")
+  temp_modQDA   = fit(classe ~ ., data = dbtreino[,c(7,temp_regress[[2]])],
+                 model = "qda", task = "c")
+  temp_modNBB   = fit(classe ~ ., data = dbtreino[,c(7,temp_regress[[3]])],
+                 model = "naiveBayes", task = "c")
+  
   
   # predicting values
-  Ypred5NN[idCV==g]  = predict(mod5NN, dbteste[,regress[[1]]])
-  Ypred10NN[idCV==g] = predict(mod10NN, dbteste[,regress[[2]]])
-  Ypred15NN[idCV==g] = predict(mod15NN, dbteste[,regress[[3]]])
-  YpredLDA[idCV==g]  = predict(modLDA, dbteste[,regress[[4]]])
-  YpredDT[idCV==g]   = predict(modDT, dbteste[,regress[[5]]])
-  YpredSVM[idCV==g]  = predict(modSVM, dbteste[,regress[[6]]])
-  YpredMULTINOM[idCV==g]  = predict(modMULTINOM, dbteste[,regress[[7]]])
+  temp_YpredLDA[temp_idCV==g]  = predict(temp_modLDA, dbteste[,temp_regress[[1]]])
+  temp_YpredQDA[temp_idCV==g]  = predict(temp_modQDA, dbteste[,temp_regress[[2]]])
+  temp_YpredNBB[temp_idCV==g]  = predict(temp_modNBB, dbteste[,temp_regress[[3]]])
   setTxtProgressBar(pb2, g)
  };close(pb2)
  # error rate
- tx_erro[i,"5NN"]  = (1 - sum(diag(prop.table(table(Y, Ypred5NN))))) * 100
- tx_erro[i,"10NN"] = (1 - sum(diag(prop.table(table(Y, Ypred10NN))))) * 100
- tx_erro[i,"15NN"] = (1 - sum(diag(prop.table(table(Y, Ypred15NN))))) * 100
- tx_erro[i,"LDA"]  = (1 - sum(diag(prop.table(table(Y, YpredLDA))))) * 100
- tx_erro[i,"DT"]   = (1 - sum(diag(prop.table(table(Y, YpredDT))))) * 100
- tx_erro[i,"SVM"]  = (1 - sum(diag(prop.table(table(Y, YpredSVM))))) * 100
- tx_erro[i,"MULTINOM"] = (1 - sum(diag(prop.table(table(Y, YpredMULTINOM))))) * 100
- setTxtProgressBar(pb, i)
-};close(pb);beepr::beep()
+ dat_txerr[i,"LDA"]  = (1 - sum(diag(prop.table(table(dat_class, temp_YpredLDA))))) * 100
+ dat_txerr[i,"QDA"]  = (1 - sum(diag(prop.table(table(dat_class, temp_YpredQDA))))) * 100
+ dat_txerr[i,"NBB"]  = (1 - sum(diag(prop.table(table(dat_class, temp_YpredNBB))))) * 100
+ setTxtProgressBar(temp_pb, i)
+};close(temp_pb);beepr::beep()
 
 print(proc.time()[3] - tm)
 
 # mc estimate
-colMeans(tx_erro)
-apply(tx_erro, 2, FUN = sd)
+colMeans(dat_txerr)
+apply(dat_txerr, 2, FUN = sd)
 
-boxplot(tx_erro)
+boxplot(dat_txerr)
+
+rm(list = ls(pattern="temp_"))
+
+save.image("loo CV for errselec vars.RData")
